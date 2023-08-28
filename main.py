9734446b650b528877a112c2ef796bc96668daea
gandhi <sagaraigandhi@gmail.com>
2023-08-28 21:01:36 +0900

    Adding secret

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #Hello World1
+
+# [default]
+# aws_access_key_id = ABDHHFJNKFNL232DMFLSN
+# aws_secret_access_key = Kf3214515kfnew42523ffsgsdgdsgfsdgsdgs52
+# output= json
+# region = us-east-2
+
+is_suuny = True
+print(is_suuny)
