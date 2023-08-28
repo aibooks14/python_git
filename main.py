4365811b7a3161098c6be227d7930e07c615f333
gandhi <sagaraigandhi@gmail.com>
2023-08-28 21:34:00 +0900

    test "4th commit"

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #Hello World1
 
 # [default]
-# aws_access_key_id = ABDHHFJNKFNL232DMFLSN
-# aws_secret_access_key = Kf3214515kfnew42523ffsgsdgdsgfsdgsdgs52
-# output= json
-# region = us-east-2
 
-# key=412rfsafa3232sr
+aws_secret_access_key = Kf3214515kfnew42523ffsgsdgdsgfsdgsdgs52
+output= json
+region = us-east-2
+
+
